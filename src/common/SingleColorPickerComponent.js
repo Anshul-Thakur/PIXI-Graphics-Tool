9ad7b3d67ffbfab386ebc458d8ed2d7b7d10d12d
Anshul-Thakur <anshulthakur0508@gmail.com>
2018-12-09 18:27:58 +0530

    adding single picker

---
+++ src/common/SingleColorPickerComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SingleColorPickerComponent extends React.Component {
+    render() {
+        return(                  
+            <div>
+                <input type="color" name="single-color-picker" onChange={(e)=> {this.props.onChange(e, this.props.data.propName)}}></input>
+            </div>
+        );
+    }
+}
+
+export default  SingleColorPickerComponent;
+
